b414fdfb674701843ad7faa7f76b3a37b15446ab
Saksham Mittal <saksham2005mittal@gmail.com>
2024-02-24 11:42:02 +0530

    small fix

--- form_maker/views.py
+++ form_maker/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from .models import *
 
 # Create your views here.
-def  index(request):
+def index(request):
     if  request.method == 'POST':
         data=request.POST
 
