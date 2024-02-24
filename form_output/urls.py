910997d3e1b3019904ae68be25bf0d3ae344f640
Shreyas Mahajan <142911768+ShreyasMahajann@users.noreply.github.com>
2024-02-24 10:38:50 +0000

    urls.py

--- form_output/urls.py
+++ form_output/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('/form_output', views.output, name="output"),
+    path('form_output/', views.output, name="output"),
 ] 
