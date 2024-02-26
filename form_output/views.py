799c65243e30e1f2c001c2b14918ec408bf7d0ac
Shreyas Mahajan <142911768+ShreyasMahajann@users.noreply.github.com>
2024-02-26 15:01:45 +0000

    output

--- form_output/views.py
+++ form_output/views.py
@@ -1,6 +1,13 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render,HttpResponse,redirect
 
 # Create your views here.
 def output(request):
-    fields = ['name']
-    return render(request,'test.html',fields)
+    if request.method=="POST":
+        data=request.POST
+        Name=data.get('Name')
+        print(Name)
+        return redirect('/form_output/')
+    # querryset=form_maker.Form.objects.all()
+    # context={'feilds':querryset}
+    context={'feilds':["Name","Age","email","Phone"]}
+    return render(request, 'test.html', context)
