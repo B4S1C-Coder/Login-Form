23637218353cc6ea4d4691621585484799f9efae
Saksham Mittal <saksham2005mittal@gmail.com>
2024-04-07 02:58:43 +0530

    Removed static folder in form_maker

--- form_maker/static/assets/javascript/home.js
+++
@@ -1,8 +0,0 @@
-
-document.querySelectorAll('.branch-option').forEach((option) => {
-  option.addEventListener('click', (event) => {
-    event.preventDefault();
-    const branchValue = event.target.dataset.value;
-    document.getElementById('branch-input').value = branchValue;
-  });
-});
