24302b911f881de72f25200cdd33d276ba8c3572
Shreyas Mahajan <142911768+ShreyasMahajann@users.noreply.github.com>
2024-04-15 12:11:39 +0000

    delete#

--- api_form_maker/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ApiFormMakerConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'api_form_maker'
--- api_form_maker/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from form_maker.models import Form
-
-class FormSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Form
-        fields = '__all__'
